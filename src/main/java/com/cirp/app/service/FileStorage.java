a77655531f4b98abd9a935449fd7f8cf6d20ffc2
Jincy P Janardhanan <jincyp7@gmail.com>
2020-10-29 03:02:15 +0530

    configured for deployment (localhost:8080) using JAR

--- src/main/java/com/cirp/app/service/FileStorage.java
+++ src/main/java/com/cirp/app/service/FileStorage.java
@@ -1,8 +1,6 @@
 package com.cirp.app.service;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,33 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileStorage {
 	
 	public static final String UPLOAD_PATH = System.getProperty("user.home") + "\\CIRP\\";
-	
-	public FileStorage() throws IOException, URISyntaxException {
-		
-		String picsfolder[] = {"//profile-pictures//", "//backgrounds//"};
-		
-		for(int i = 0; i<2; i++) {
-			File folder = new File(UPLOAD_PATH + picsfolder[i]);
-			if (!folder.exists()) {
-				folder.mkdirs();
-			}
-		}
-		
-		String source_filename[] = {"college.png", "company.png", "students.png", "jobs.png", "default_background.png"};
-		String dest_filename[] = {"default_college.png", "default_recruiter.png", "default_student.png", "default_job.png", "default_background.png"};
-		
-		for(int i = 0; i<5; i++) {
-			Path sourcepath = Paths.get(ClassLoader.getSystemResource("static/css/assets/" + source_filename[i]).toURI());
-			Path destinationpath;
-			if(i != 4) {
-				destinationpath = Paths.get(UPLOAD_PATH + picsfolder[0] + dest_filename[i]);
-			} else {
-				destinationpath = Paths.get(UPLOAD_PATH + picsfolder[1] + dest_filename[i]);
-			}
-			Files.copy(sourcepath,destinationpath, StandardCopyOption.REPLACE_EXISTING);
-		}
-		
-	}
 
 	public static String storeFile(MultipartFile file, String username, String foldername) {
 
